da419769814d607427c2f32c95706872832c9a1f
Jeremie Regnier <regnjere@gmail.com>
2021-02-28 15:06:07 +0000

    input and output

--- scripts/inout.js
+++ scripts/inout.js
@@ -1,41 +1,42 @@
+'use strict';
+function MutationToDom() {
+  var container = document.createElement('mutation');
+  var continueOnError = (this.getFieldValue('HasCONTINUE') == 'TRUE');
+  container.setAttribute('continueOnError', continueOnError);
+  return container;
+}
+
+function DomToMutation(xmlElement) {
+  var continueOnError = (xmlElement.getAttribute('continueOnError') == 'true');
+  this.updateShape_(continueOnError);
+}
 Blockly.Blocks['text_print2'] = {
-  /**
-   * Block for print statement.
-   * @this {Blockly.Block}
-   */
-  init: function() {
-    this.jsonInit({
-      "message0": Blockly.Msg['TEXT_PRINT_TITLE'],
-      "args0": [
-        {
-          "type": "input_value",
-          "name": "TEXT"
-        }
-      ],
-      "previousStatement": null,
-      "nextStatement": null,
-      "style": "text_blocks",
-      "tooltip": Blockly.Msg['TEXT_PRINT_TOOLTIP'],
-      "helpUrl": Blockly.Msg['TEXT_PRINT_HELPURL']
-    });
+  init: function () {
+    this.appendValueInput("TEXT")
+      .setCheck(null)
+      .appendField("print");
+    this.setInputsInline(false);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(160);
+    this.setTooltip("");
+    this.setHelpUrl("");
   }
 };
 
-Blockly.JavaScript['text_print2'] = function(block) {
-  // Print statement.
-  var msg = Blockly.JavaScript.valueToCode(block, 'TEXT',
-      Blockly.JavaScript.ORDER_NONE) || '\'\'';
-  msg = msg.replaceAll("'","")
+Blockly.JavaScript['text_print2'] = function (block) {
+  var value_text = Blockly.JavaScript.valueToCode(block, 'TEXT', Blockly.JavaScript.ORDER_FUNCTION_CALL);
   
-  return '$("#inout").html($("#inout").html()+"'+msg+'");\n';
+  //return '$("#inout").append("<p>'+value_text +'</p>");';
+  return '$("#inout").append("<p>"); $("#inout").append('+value_text+'); $("#inout").append("</p>");';
 };
 
 Blockly.Blocks['text_prompt_ext2'] = {
   /**
    * Block for prompt function (external message).
    * @this {Blockly.Block}
-   */
-  init: function() {
+  */
+  init: function () {
     var TYPES = [
       [Blockly.Msg['TEXT_PROMPT_TYPE_TEXT'], 'TEXT'],
       [Blockly.Msg['TEXT_PROMPT_TYPE_NUMBER'], 'NUMBER']
@@ -44,48 +45,36 @@ Blockly.Blocks['text_prompt_ext2'] = {
     this.setStyle('text_blocks');
     // Assign 'this' to a variable for use in the closures below.
     var thisBlock = this;
-    var dropdown = new Blockly.FieldDropdown(TYPES, function(newOp) {
+    var dropdown = new Blockly.FieldDropdown(TYPES, function (newOp) {
       thisBlock.updateType_(newOp);
     });
     this.appendValueInput('TEXT')
-        .appendField(dropdown, 'TYPE');
+      .appendField(dropdown, 'TYPE');
     this.setOutput(true, 'String');
-    this.setTooltip(function() {
+    this.setTooltip(function () {
       return (thisBlock.getFieldValue('TYPE') == 'TEXT') ?
-          Blockly.Msg['TEXT_PROMPT_TOOLTIP_TEXT'] :
-          Blockly.Msg['TEXT_PROMPT_TOOLTIP_NUMBER'];
+        Blockly.Msg['TEXT_PROMPT_TOOLTIP_TEXT'] :
+        Blockly.Msg['TEXT_PROMPT_TOOLTIP_NUMBER'];
     });
   },
-  /**
-   * Modify this block to have the correct output type.
-   * @param {string} newOp Either 'TEXT' or 'NUMBER'.
-   * @private
-   * @this {Blockly.Block}
-   */
-  updateType_: function(newOp) {
+  updateType_: function (newOp) {
     this.outputConnection.setCheck(newOp == 'NUMBER' ? 'Number' : 'String');
   },
-  /**
-   * Create XML to represent the output type.
-   * @return {!Element} XML storage element.
-   * @this {Blockly.Block}
-   */
-  mutationToDom: function() {
+  
+  MutationToDom() {
+    
     var container = Blockly.utils.xml.createElement('mutation');
     container.setAttribute('type', this.getFieldValue('TYPE'));
     return container;
   },
-  /**
-   * Parse XML to restore the output type.
-   * @param {!Element} xmlElement XML storage element.
-   * @this {Blockly.Block}
-   */
-  domToMutation: function(xmlElement) {
+
+  DomToMutation(xmlElement) {
+    
     this.updateType_(xmlElement.getAttribute('type'));
   }
 };
 
-Blockly.JavaScript['text_prompt_ext2'] = function(block) {
+Blockly.JavaScript['text_prompt_ext2'] = function (block) {
   // Prompt function.
   if (block.getField('TEXT')) {
     // Internal message.
@@ -93,12 +82,16 @@ Blockly.JavaScript['text_prompt_ext2'] = function(block) {
   } else {
     // External message.
     var msg = Blockly.JavaScript.valueToCode(block, 'TEXT',
-        Blockly.JavaScript.ORDER_NONE) || '\'\'';
+      Blockly.JavaScript.ORDER_NONE) || '\'\'';
   }
-  var code = 'window.prompt(' + msg + ')';
+
+  var code = 'window.prompt('+msg+')' 
   var toNumber = block.getFieldValue('TYPE') == 'NUMBER';
   if (toNumber) {
-    code = 'Number(' + code + ')';
+    code = 'Number('+code+')';
   }
+  var code = '(function(){$("#inout").append("<span>");\n $("#inout").append('+msg+');\n $("#inout").append(" </span>");\n $("#inout").append('+code+'); return '+code+'})();'
   return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
 };
+
+
