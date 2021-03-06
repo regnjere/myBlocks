42989e588720fc825227a977e836543352625ea5
Jeremie Regnier <regnjere@gmail.com>
2021-03-06 14:37:24 +0000

    Adjusted displays (turtle, text, code) and turtle size based off of screen size

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,19 @@
 'use strict';
 
 var myWorkspace = null;
-
-
-//Disable blocks that need to be imported to work
 myWorkspace = Blockly.inject('blocklyDiv',
-  {
-    toolbox: document.getElementById('toolbox-categories'),
-  });
-  $(function(){
-    $("[name='Turtle'] > *").attr("disabled",true);
-    $("[type='math_trig']").attr("disabled",true);
-    $("[type='math_constant']").attr("disabled",true);
-    $("[type='math_atan2']").attr("disabled",true);
-    $("[type='math_random_int']").attr("disabled",true);
-    $("[type='math_random_float']").attr("disabled",true);
-    $("[type='colour_random']").attr("disabled",true);
-    setTimeout(BlocklyStorage.restoreBlocks, 0);  
+{
+  toolbox: document.getElementById('toolbox-categories'),
+});
+$(function(){
+
+  //Disable blocks that need to be imported to work
+  $("[name='Turtle'] > *").attr("disabled",true);
+  $("[type='math_trig']").attr("disabled",true);
+  $("[type='math_constant']").attr("disabled",true);
+  $("[type='math_atan2']").attr("disabled",true);
+  $("[type='math_random_int']").attr("disabled",true);
+  $("[type='math_random_float']").attr("disabled",true);
+  $("[type='colour_random']").attr("disabled",true);
+  setTimeout(BlocklyStorage.restoreBlocks, 0);  
 });
